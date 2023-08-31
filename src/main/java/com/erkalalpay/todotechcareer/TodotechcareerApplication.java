988e8b53ff950460b3a8f569b0666cd3c06cd136
Erkal Alpay <erkalalpay98@hotmail.com>
2023-09-01 02:01:41 +0300

    first commit

---
+++ src/main/java/com/erkalalpay/todotechcareer/TodotechcareerApplication.java
@@ -0,0 +1,13 @@
+package com.erkalalpay.todotechcareer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodotechcareerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodotechcareerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erkalalpay/todotechcareer/TodotechcareerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erkalalpay.todotechcareer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodotechcareerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

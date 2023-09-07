f52310a902754ee177674985a2c7a06f4d9f317a
Erkal Alpay <erkalalpay98@hotmail.com>
2023-09-07 14:01:59 +0300

    Todo Check

--- src/main/java/com/erkalalpay/todotechcareer/Repository/TodoRepository.java
+++ src/main/java/com/erkalalpay/todotechcareer/Repository/TodoRepository.java
@@ -2,11 +2,13 @@ package com.erkalalpay.todotechcareer.Repository;
 
 import com.erkalalpay.todotechcareer.Entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface TodoRepository extends JpaRepository <Todo, Long> {
+
     Todo getById(Long id);
 
 }
--- todoreact/src/components/TodoList.js
+++ todoreact/src/components/TodoList.js
@@ -1,25 +1,129 @@
 import React, { useEffect, useState } from 'react'
-import TodoService from '../Service/TodoService';
+import axios from "axios";
+
 
 export default function TodoList() {
 
+
+
   //STATE
-  const [TodoList, setTodoList] = useState();
+  const [TodoList, setTodoList] = useState([]);
 
   //USE.EFFECT
-  useEffect(()=>{
-    TodoService.saveTodo.
-    then((response)=>{
-      console.log(response.data);
-    })
-    .catch((err)=>{console.error(err);})
+  useEffect(() => {
+    axios.get("http://localhost:8080/")
+      .then((response) => {
+        console.log(response.data);
+        setTodoList(response.data);
+      })
+      .catch((err) => { console.error(err); })
 
+  }, []);
+
+  //LIST
+
+  // const getList = (()=>{
+  //   axios.get("http://localhost:8080/")
+  //   .then((response)=>{
+  //   console.log(response.data);
+  //   setTodoList(response.data);
+  //   })
+  //   .catch((err)=>{console.error(err);})
+
+  // });
+
+  //UPDATE
+
+  const setUpdateList = (data) => {
+    let { id, todoName } = data;
+    localStorage.setItem("todo_update_id", id);
+    localStorage.setItem("todo_update_todoName", todoName);
+  }
+
+  //CHECKBOX
+
+
+
+  //DELETE
+  const setDeleteTodos = (() => {
+
+    if (window.confirm("silmek istediğine emin misin")) {
+      axios.get("http://localhost:8080/deleteall")
+        .then(() => {
+          getList();
+
+        })
+        .catch((err) => { console.error(err); })
+    }
+    else {
+      alert("Silinmedi")
+    }
   });
+
+
+
+  //VIEW
+  const getList = (() => {
+    axios.get("http://localhost:8080/")
+      .then((response) => {
+        console.log(response.data);
+        console.log(response.headers);
+        setTodoList(response.data);
+      })
+      .catch((err) => { console.error(err); })
+  });
+
+  //SELECT
+
+  const setSelectTodo = (id) => {
+    localStorage.setItem("todo_select", id);
+  }
+
 
   //RETURN
   return (
 
-    <div>TodoList</div>
+    <>
+
+      <h1 className="text-center display-4"><b>To-Do's</b></h1>
+
+      <table class="table">
+        <thead>
+          <tr>
+            <th>To-Do's</th>
+            <th>Description</th>
+            <th>Check</th>
+            <th>Update</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            TodoList.map((data) =>
+              <tr key={data.id}>
+                <td>{data.id}</td>
+                <td>{data.todo}</td>
+                <td>
+                  <input type='checkbox' id="abc" checked={data.isCompleted} />
+                  <label for="abc">
+                    {(data.isCompleted) ? "Tamamlandı" : "Tamamlanmadı"}
+                  </label>
+                </td>
+
+                <td>
+                  {/* <Link to={getList()}>
+                     <i onClick={()=> getList(data.id)} className='fa-solid fa-expand text-warning'> </i>
+                  </Link> */}
+
+                </td>
+
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+
+    </>
 
   )
 

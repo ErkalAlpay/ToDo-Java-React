4d9d07fb3d7045d40547979abad91feffc32ad51
Erkal Alpay <erkalalpay98@hotmail.com>
2023-09-12 01:08:52 +0300

    Register form request

--- src/main/java/com/erkalalpay/todotechcareer/Model/Request/RegisterFormRequest.java
+++ src/main/java/com/erkalalpay/todotechcareer/Model/Request/RegisterFormRequest.java
@@ -1,9 +1,22 @@
 package com.erkalalpay.todotechcareer.Model.Request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.NonNull;
+
 
 @Data
 public class RegisterFormRequest {
+    @NotEmpty
+    @NotNull
+    @Email
+    @Size(min = 8)
     String email;
+    @NotEmpty
+    @NotNull
+    @Size(min = 8)
     String password;
 }

d0a3b2fc0e2313494b2c149dbbdbfc7791a59f04
Erkal Alpay <erkalalpay98@hotmail.com>
2023-09-12 01:20:37 +0300

    Base Service

--- src/main/java/com/erkalalpay/todotechcareer/Base/BaseServiceImp.java
+++ src/main/java/com/erkalalpay/todotechcareer/Base/BaseServiceImp.java
@@ -1,4 +1,29 @@
 package com.erkalalpay.todotechcareer.Base;
 
-public interface BaseServiceImp {
+import java.util.List;
+
+// D: Dto
+// E: Entity
+public interface BaseServiceImp <D, E> {
+    // Model Mapper
+    public D entityToDto(E e);
+
+    public E dtoToEntity(D d);
+
+    // C R U D
+    // CREATE
+    public D create(D d);
+
+    // LIST
+    public List<D> list();
+
+    // FIND BY
+    public D findById(Long id);
+
+    // UPDATE
+    public D update(Long id,D d);
+
+    // DELETE
+    public D deleteById(Long id);
 }
+

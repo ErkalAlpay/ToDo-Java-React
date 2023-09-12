3a3a4e18b7bd060330e9c3d3f5dbf4b072322b23
Erkal Alpay <erkalalpay98@hotmail.com>
2023-09-12 14:23:53 +0300

    JWT Token

---
+++ src/main/java/com/erkalalpay/todotechcareer/Service/JwtTokenService.java
@@ -0,0 +1,28 @@
+package com.erkalalpay.todotechcareer.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtTokenService {
+
+    private final static String secretKey= "secretKey";
+    private final Algorithm algorithm= Algorithm.HMAC512(secretKey);
+
+    public String generateToken(String email){
+        String token = JWT.
+                create().
+                withSubject(email).
+                sign(algorithm);
+        return token;
+    }
+
+    public String getTokenMail(String token){
+        DecodedJWT decodedJWT = JWT.require(algorithm).build().verify(token);
+        return decodedJWT.getSubject();
+    }
+
+
+}
--- src/main/java/com/erkalalpay/todotechcareer/Service/UserService.java
+++ src/main/java/com/erkalalpay/todotechcareer/Service/UserService.java
@@ -1,14 +1,22 @@
 package com.erkalalpay.todotechcareer.Service;
 
 import com.erkalalpay.todotechcareer.Base.BaseServiceImp;
+import com.erkalalpay.todotechcareer.Configuration.BeanConfig;
 import com.erkalalpay.todotechcareer.Model.Dto.UserDTO;
 import com.erkalalpay.todotechcareer.Model.Entity.User;
+import com.erkalalpay.todotechcareer.Repository.UserRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 
 @Service
 public class UserService implements BaseServiceImp <UserDTO, User> {
+
+    //Calls
+    private BeanConfig beanConfig;
+
+    private UserRepository userRepository;
+
     @Override
     public UserDTO entityToDto(User user) {
         return null;
@@ -21,7 +29,11 @@ public class UserService implements BaseServiceImp <UserDTO, User> {
 
     @Override
     public UserDTO create(UserDTO userDTO) {
-        return null;
+        User user = beanConfig.modelMapperBean().map(userDTO, User.class);
+        String bcryptedPassword = beanConfig.bCryptPasswordEncoder().encode(user.getPassword());
+        user.setPassword(bcryptedPassword);
+        userRepository.save(user);
+        return
     }
 
     @Override

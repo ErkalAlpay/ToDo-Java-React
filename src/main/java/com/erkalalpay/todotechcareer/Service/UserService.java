f24e82f95a49a521c69037e3fbfbd5ba817d5e62
Erkal Alpay <erkalalpay98@hotmail.com>
2023-09-12 12:50:19 +0300

    Update UserService.java

--- src/main/java/com/erkalalpay/todotechcareer/Service/UserService.java
+++ src/main/java/com/erkalalpay/todotechcareer/Service/UserService.java
@@ -1,4 +1,43 @@
 package com.erkalalpay.todotechcareer.Service;
 
-public class UserService {
+import com.erkalalpay.todotechcareer.Base.BaseServiceImp;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService implements BaseServiceImp {
+    @Override
+    public Object entityToDto(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object dtoToEntity(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object create(Object o) {
+        return null;
+    }
+
+    @Override
+    public List list() {
+        return null;
+    }
+
+    @Override
+    public Object findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Object update(Long id, Object o) {
+        return null;
+    }
+
+    @Override
+    public Object deleteById(Long id) {
+        return null;
+    }
 }

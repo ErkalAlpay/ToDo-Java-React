fe3ae2f26555accf8e1d7a9e1358adbcbf299ad8
Erkal Alpay <erkalalpay98@hotmail.com>
2023-09-13 20:27:22 +0300

    Update SecurityConfiguration.java

--- src/main/java/com/erkalalpay/todotechcareer/Configuration/SecurityConfiguration.java
+++ src/main/java/com/erkalalpay/todotechcareer/Configuration/SecurityConfiguration.java
@@ -2,8 +2,10 @@ package com.erkalalpay.todotechcareer.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,7 +14,12 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests().anyRequest().permitAll();
-        return http.build();
+        http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest()
+                .authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
+               return http.build();
+
     }
 }

d1c70dc0c61515b8e4b9e195897be26ebbac7182
Erkal Alpay <erkalalpay98@hotmail.com>
2023-09-13 19:15:31 +0300

    Update UserController.java

--- src/main/java/com/erkalalpay/todotechcareer/Controller/UserController.java
+++ src/main/java/com/erkalalpay/todotechcareer/Controller/UserController.java
@@ -2,7 +2,6 @@ package com.erkalalpay.todotechcareer.Controller;
 
 import com.erkalalpay.todotechcareer.Model.Request.RegisterFormRequest;
 import com.erkalalpay.todotechcareer.Service.UserService;
-import org.modelmapper.internal.bytebuddy.implementation.bytecode.Throw;
 import org.springframework.stereotype.Controller;
 
 @Controller

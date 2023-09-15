1db9124092217a04a7a285fb8aa64b487377df3b
Erkal Alpay <erkalalpay98@hotmail.com>
2023-09-15 14:24:12 +0300

    Update UserLogin.js

--- todoreact/src/components/UserLogin.js
+++ todoreact/src/components/UserLogin.js
@@ -1,20 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UserService from '../Service/UserService';
 
 export default function UserLogin() {
+
      //STATE
-     const [loginEmail, setLoginEmail] = useState();
-     const [loginPassword, setLoginPassword] = useState();
+     const [loginEmail, setLoginEmail] = useState("");
+     const [loginPassword, setLoginPassword] = useState("");
+     const [error, setError] = useState();
 
 
+    const userService = new UserService();
 
 
+     const loginUser = async (event) => {
+        event.preventDefault();
 
+        // Login Metot
+            const request = {
+                "email": loginEmail,
+                "password": loginPassword
+            };
+            console.log(request);
 
+            //SERVICE
+            try {
+                const response = await userService.userLogin(request);
+                console.log(response);
+            //Burada response ile ne yapmak istediğinizi ekleyebilirsiniz
+               
+            } catch (err) {
+                setError("");
+            }
+    }
 
+    // EVENT LISTENERS
+    const emailOnChange = (event) => {
+        const { loginEmail, value } = event.target;
+        // onChange
+        setLoginEmail(value)
+        
+    }
 
-
-
-
+    const passwordOnChange = (event) => {
+        const { loginPassword, value } = event.target;
+        // onChange
+        setLoginPassword(value)
+    }
 
 
 
@@ -36,7 +67,7 @@ export default function UserLogin() {
                                             <div className="d-flex flex-row align-items-center mb-4">
                                                 <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                                 <div className="form-outline flex-fill mb-0">
-                                                    <input type="email" id="form3Example3c" className="form-control" />
+                                                    <input type="email" id="form3Example3c" className="form-control" onChange={emailOnChange} />
                                                     <label className="form-label" htmlFor="form3Example3c">Your Email</label>
                                                 </div>
                                             </div>
@@ -44,13 +75,13 @@ export default function UserLogin() {
                                             <div className="d-flex flex-row align-items-center mb-4">
                                                 <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
                                                 <div className="form-outline flex-fill mb-0">
-                                                    <input type="password" id="form3Example4c" className="form-control" />
+                                                    <input type="password" id="form3Example4c" className="form-control" onChange={passwordOnChange} />
                                                     <label className="form-label" htmlFor="form3Example4c">Password</label>
                                                 </div>
                                             </div>                    
 
                                             <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                                <button type="button" className="btn btn-primary btn-lg">Login</button>
+                                                <button type="button" className="btn btn-primary btn-lg" onClick={loginUser}>Login</button>
                                             </div>
                                             <p className="text-center text-muted mt-5 mb-0">Don't you have an account yet? <a href="http://localhost:3000/register"
                                                 className="fw-bold text-body"><u>Register here</u></a></p>

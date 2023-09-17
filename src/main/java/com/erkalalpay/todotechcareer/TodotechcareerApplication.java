5bee567a2a53fb714961df63f66b67e91b44ec73
Erkal Alpay <erkalalpay98@hotmail.com>
2023-09-17 17:31:12 +0300

    Trying to implement Swagger

--- src/main/java/com/erkalalpay/todotechcareer/TodotechcareerApplication.java
+++ src/main/java/com/erkalalpay/todotechcareer/TodotechcareerApplication.java
@@ -1,8 +1,12 @@
 package com.erkalalpay.todotechcareer;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,4 +16,12 @@ public class TodotechcareerApplication {
 		SpringApplication.run(TodotechcareerApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI baseOpenAPI(){
+		return new OpenAPI()
+				.info(new Info()
+						.title("Erkal To-Do API")
+						.version("1.0.0").description("Doc Description")
+						.license(new License().name("Alpay License")));
+	}
 }
